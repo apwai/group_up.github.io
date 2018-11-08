6300cbb35fbe4a7cf5e6911e7a11669bcf9b72fd
Alice Lee <aliceelee98@gmail.com>
2018-11-08 15:38:35 -0800

    add

--- html_skeletons/scripts/information.js
+++ html_skeletons/scripts/information.js
@@ -5,8 +5,8 @@ var accountInfo = {'username': 'fan123',
                    'event2': 'Boxing Class',
                    'index': 1};
 
-//Fake complex database
-var complexAccount= [
+//Fake  database
+var Account= [
   {'username': 'soccer123', 'password': 'ball123', 'event1': 'Soccer Class', 'event2': 'Gymming Kids','index': 2},
   {'username': 'art123', 'password': 'class123', 'event1': 'Drawing', 'event2': 'Wine and Dine','index': 3},
   {'username': 'baller123', 'password': 'game123', 'event1': 'Beginner Yoga', 'event2': 'Wine and Dine','index': 4},
@@ -19,16 +19,16 @@ var complexAccount= [
 
 //var eventInfo = {'actName': 'Soccer Class', 'startDate': 'Oct 3rd 2018', 'endDate': 'Oct 23rd 2018', 'address': '123 Birch St', 'numberPeople': '6', 'eventNum': 1}
 
-//FAKE complex database for events
+//FAKE  database for events
 
-var complexEvent = [
-  {'actName': 'Drawing', 'startDate': 'Nov 3rd 2018', 'endDate': 'Nov 23rd 2018', 'address': '123 Blue St', 'numberPeople': '14', 'description': 'MasterClasses are 3-hour painting workshops that focus on an artist or technique using Right-brain painting techniques. Each MasterClass begins with an information slideshow and lecture before artists move onto painting. Dance to music and create fun pieces that you’ll want to hang in your house.', 'eventNum': 2},
-  {'actName': 'Beginner Yoga', 'startDate': 'Nov 20th 2018', 'endDate': 'Nov 30th 2018', 'address': '123 Green St', 'numberPeople': '6', 'description': 'This class is designed for first time yogis. It is the right class for you if you want to learn the fundamentals of yoga in a group where everyone is new. The teacher will give you some background on yoga while you are seated, but come prepared to move. You’ll walk out feeling embodied, empowered, energized, and eager to come back.', 'eventNum': 3},
-  {'actName': 'Wine and Dine', 'startDate': 'Oct 3rd 2018', 'endDate': 'Oct 23rd 2018', 'address': '1245 Lebon Drive', 'numberPeople': '6', 'description': 'a fun class', 'eventNum': 4},
-  {'actName': 'Basketball Tournament', 'startDate': 'Oct 3rd 2018', 'endDate': 'Oct 23rd 2018', 'address': '3458 Rock St', 'numberPeople': '6', 'description': 'a great class', 'eventNum': 5},
-  {'actName': 'Boxing Class', 'startDate': 'Dec 3rd 2018', 'endDate': 'Dec 23rd 2018', 'address': 'Boxing Club', 'numberPeople': '6', 'description': 'very fun', 'eventNum': 6},
-  {'actName': 'Basketball 5 on 5', 'startDate': 'Oct 3rd 2018', 'endDate': 'Oct 23rd 2018', 'address': 'Amazon Park', 'numberPeople': '6', 'description': 'great', 'eventNum': 7},
-  {'actName': 'Gymming Kids', 'startDate': 'Nov 14th 2018', 'endDate': 'Nov 20th 2018', 'address': 'RIMAC', 'numberPeople': '6', 'description': 'nice', 'eventNum': 8}
+var fakePosts = [
+  {'actName': 'Drawing', 'startTime': 'Nov 3rd 2018 4PM', 'endTime': 'Nov 23rd 2018 7PM', 'address': '123 Blue St', 'numPeople': '14', 'description': 'MasterClasses are 3-hour painting workshops that focus on an artist or technique using Right-brain painting techniques. Each MasterClass begins with an information slideshow and lecture before artists move onto painting. Dance to music and create fun pieces that you’ll want to hang in your house.', 'creator': 'fakeUser'},
+  {'actName': 'Beginner Yoga', 'startTime': 'Nov 20th 2018 3PM', 'endDate': 'Nov 30th 2018 5PM', 'address': '123 Green St', 'numPeople': '6', 'description': 'This class is designed for first time yogis. It is the right class for you if you want to learn the fundamentals of yoga in a group where everyone is new. The teacher will give you some background on yoga while you are seated, but come prepared to move. You’ll walk out feeling embodied, empowered, energized, and eager to come back.', 'creator': 'fakeUser'},
+  {'actName': 'Wine and Dine', 'startTime': 'Oct 3rd 2018', 'endTime': 'Oct 23rd 2018', 'address': '1245 Lebon Drive', 'numPeople': '6', 'description': 'a fun class', 'creator': 'fakeUser'},
+  {'actName': 'Basketball Tournament', 'startTime': 'Oct 3rd 2018', 'endTime': 'Oct 23rd 2018', 'address': '3458 Rock St', 'numbPeople': '6', 'description': 'a great class', 'creator': 'fakeUser'},
+  {'actName': 'Boxing Class', 'startTime': 'Dec 3rd 2018', 'endTime': 'Dec 23rd 2018', 'address': 'Boxing Club', 'numPeople': '6', 'description': 'very fun', 'creator': 'fakeUser'},
+  {'actName': 'Basketball 5 on 5', 'startTime': 'Oct 3rd 2018', 'endTime': 'Oct 23rd 2018', 'address': 'Amazon Park', 'numPeople': '6', 'description': 'great', 'creator': 'fakeUser'},
+  {'actName': 'Gymming Kids', 'startTime': 'Nov 14th 2018', 'endTime': 'Nov 20th 2018', 'address': 'RIMAC', 'numPeople': '6', 'description': 'nice', 'creator': 'fakeUser'}
 ]
 
 //Call this function when page loads
@@ -46,9 +46,9 @@ $(document).ready(function() {
   // console.log(html);
   // parentDiv.append(html);
 
-  //iterate through complex info
-  for (var i= 0; i < complexEvent.length; i++) {
-    var curData= complexEvent[i];
+  //iterate through  info
+  for (var i= 0; i < fakePosts.length; i++) {
+    var curData= fakePosts[i];
     var curHtml = template(curData);
     parentDiv.append(curHtml);
   }
